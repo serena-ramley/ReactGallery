98dd800ee130a746423e0b5bf94b0a32dfe9e7fa
serena-ramley <sramley@gmail.com>
2020-01-06 12:28:41 -0800

    Reformatted

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const SearchBox = () => {
     return (
-        <input type='search' placeholder="search for restaurants..." />
+        <div>
+            <input
+                type='search'
+                placeholder="search for restaurants..."
+            />
+        </div>
     )
 }
 
