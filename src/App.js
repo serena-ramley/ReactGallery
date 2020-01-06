b439753c16f680bbd283f883768345d51e10f7bc
serena-ramley <sramley@gmail.com>
2020-01-06 13:28:57 -0800

    Updated App to filter cards based on search by name

--- src/App.js
+++ src/App.js
@@ -8,27 +8,28 @@ class App extends Component {
         super()
         this.state = {
             cards: cards,
-            searchfield: '',
+            searchField: '',
         }
     }
 
     onSearchChange = (event) => {
         this.setState( {searchField: event.target.value})
-        const filteredCards = this.state.cards.filter(card => {
-            return (card.name.toLowerCase().includes(this.state.searchField.toLowerCase())
-                    || card.food.toLowerCase().includes(this.state.searchField.toLowerCase())
-            )
-        })
-        console.log(filteredCards);
+        //console.log(filteredCards);
     }
 
+
+//                    || card.food.toLowerCase().includes(this.state.searchField.toLowerCase())
+
     render () {
+        const filteredCards = this.state.cards.filter(card => {
+            return (card.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+        })
         return (
             <div className="tc">
                 <h1> Featured Restaurants with Weekly Discounts</h1>
                 <p> <i>Note: This app is for demo purposes of a gallery-style app only (not a comprehensive business review app), and information on this page is not factual. Any resemblances to real or fictional restaurants is a coincidence.</i></p>
                 <SearchBox searchChange={this.onSearchChange}/>
-                <CardList cards = {this.state.cards} />
+                <CardList cards = {filteredCards} />
             </div>
         )
     }
