fe38953accde647351e6c3eaddb3dbecdad8d5e7
serena-ramley <sramley@gmail.com>
2020-01-05 21:53:27 -0800

    Added image url based on id

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const Card = () => {
+const Card = (props) => {
     return (
         // Use tachyons to create green background and grow upon hover
-        <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='robot' src='https://robohash.org/test?200x200' />
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img className='br-pill' alt={props.food} src={`http://lorempixel.com/200/200/food/${props.id}`} width='200' height='200' />
             <div>
-                <h2>Jane Doe</h2>
-                <p>jane.doe@gmail.com</p>
+                <h2>{props.name}</h2>
+                <p>{props.city}</p>
+                <p><i>{props.days_valid}</i></p>
             </div>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Hello from './Hello';
 import Card from './Card';
+import { cards } from './cards';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
-//ReactDOM.render(<Hello greeting={'Hello React Ninja'}/>, document.getElementById('root'));
 ReactDOM.render(
         <div>
-            <Card />
-            <Card />
+            <Card id={cards[0].id} name={cards[0].name} food={cards[0].food} city={cards[0].city} days_valid={cards[0].days_valid}/>
+            <Card id={cards[1].id} name={cards[1].name} food={cards[1].food} city={cards[1].city} days_valid={cards[1].days_valid}/>
+            <Card id={cards[2].id} name={cards[2].name} food={cards[2].food} city={cards[2].city} days_valid={cards[2].days_valid}/>
             </div>
             , document.getElementById('root'));
 
