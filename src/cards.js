c5456ad8d843a2d9dec6a5ad4e94ca83c9330b16
serena-ramley <sramley@gmail.com>
2020-01-05 21:52:57 -0800

    Created default info

---
+++ src/cards.js
@@ -0,0 +1,25 @@
+export const cards = [
+    {
+        id: 8,
+        name: 'Honour Roll',
+        city: 'North Vancouver',
+        food: 'sushi',
+        days_valid: ['Tuesdays']
+    },
+
+    {
+        id: 10,
+        name: "Aunt Lolita's Bakery",
+        city: 'Vancouver',
+        food: 'bread',
+        days_valid: ['Thursdays ', ' & Fridays']
+    },
+
+    {
+        id: 3,
+        name: 'Family Kitchen',
+        city: 'Richmond',
+        food: 'appetizer',
+        days_valid: ['Mondays']
+    },
+]
