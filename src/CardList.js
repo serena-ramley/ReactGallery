2c5f05d9955c0239eca751c7638abbaac39aae44
serena-ramley <sramley@gmail.com>
2020-01-06 12:17:40 -0800

    Refactored code

--- src/CardList.js
+++ src/CardList.js
@@ -3,12 +3,22 @@ import Card from './Card';
 import { cards } from './cards';
 
 const CardList = ({cards}) => {
-    const cardComponent = cards.map( (card, i) => {
-        return <Card key={i} id={cards[i].id} name={cards[i].name} food={cards[i].food} city={cards[i].city} days_valid={cards[i].days_valid} />
-    })
     return (
         <div>
-            {cardComponent}
+            {
+                cards.map( (card, i) => {
+                    return (
+                        <Card
+                        key={i}
+                        id={cards[i].id}
+                        name={cards[i].name}
+                        food={cards[i].food}
+                        city={cards[i].city}
+                        days_valid={cards[i].days_valid}
+                        />
+                    )
+                })
+            }
         </div>
     );
 }
