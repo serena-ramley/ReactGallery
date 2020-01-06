565c7ed6302bcb5c011c1bf57fbecdd78e6dee90
serena-ramley <sramley@gmail.com>
2020-01-06 13:04:14 -0800

    Updated SearchBox and onSearchChange to console.log the search value

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardList from './CardList';
 import {cards} from './cards';
 import SearchBox from './SearchBox'
 
-const App = () => {
-    return (
-        <div className="tc">
-            <h1> Restaurants with Weekly Discounts</h1>
-            <SearchBox />
-            <CardList cards = {cards} />
-        </div>
-    )
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            cards: cards,
+            searchfield: '',
+        }
+    }
+
+    onSearchChange(event) {
+        console.log(event.target.value);
+    }
+
+    render () {
+        return (
+            <div className="tc">
+                <h1> Featured Restaurants with Weekly Discounts</h1>
+                <p> <i>Note: This app is for demo purposes of a gallery-style app only (not a comprehensive business review app), and information on this page is not factual. Any resemblances to real or fictional restaurants is a coincidence.</i></p>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList cards = {this.state.cards} />
+            </div>
+        )
+    }
 }
 
 export default App;
