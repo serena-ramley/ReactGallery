642b360b0fceefc1fba7d6abc40de9a8eb744824
serena-ramley <sramley@gmail.com>
2020-01-06 12:27:13 -0800

    Added a title and search box

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CardList from './CardList';
+import {cards} from './cards';
+import SearchBox from './SearchBox'
+
+const App = () => {
+    return (
+        <div class="tc">
+            <h1> Restaurants with Weekly Discounts</h1>
+            <SearchBox />
+            <CardList cards = {cards} />
+        </div>
+    )
+}
+
+export default App;
