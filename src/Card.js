99eecfeef7f41bf97234e4b12bd8112c513d12a7
serena-ramley <sramley@gmail.com>
2020-01-05 21:55:58 -0800

    Destructured props

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const Card = (props) => {
+const Card = ( {food, id, name, city, days_valid}) => {
     return (
         // Use tachyons to create green background and grow upon hover
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img className='br-pill' alt={props.food} src={`http://lorempixel.com/200/200/food/${props.id}`} width='200' height='200' />
+            <img className='br-pill' alt={food} src={`http://lorempixel.com/200/200/food/${id}`} width='200' height='200' />
             <div>
-                <h2>{props.name}</h2>
-                <p>{props.city}</p>
-                <p><i>{props.days_valid}</i></p>
+                <h2>{name}</h2>
+                <p>{city}</p>
+                <p><i>{days_valid}</i></p>
             </div>
         </div>
     )
