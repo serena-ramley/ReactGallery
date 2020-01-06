abbda5436704f5a8e8aaf52355a9c8bd4d260ea1
serena-ramley <sramley@gmail.com>
2020-01-06 13:53:59 -0800

    Added styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
 import {cards} from './cards';
-import SearchBox from './SearchBox'
+import SearchBox from './SearchBox';
+import './App.css';
 
 class App extends Component {
     constructor() {
@@ -29,8 +30,8 @@ class App extends Component {
         })
         return (
             <div className="tc">
-                <h1> Featured Restaurants with Weekly Discounts</h1>
-                <p> <i>Note: This app is for demo purposes of a gallery-style app only (not a comprehensive business review app), and information on this page is not factual. Any resemblances to real or fictional restaurants is a coincidence.</i></p>
+                <h1 className="f1"> Featured Restaurants with Weekly Discounts</h1>
+                <p> <i>Note: This app is to demo a gallery-style app.</i></p>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <CardList cards = {filteredCards} />
             </div>
