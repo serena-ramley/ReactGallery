90d2c239c0c820221619b41135002e0d85611f8b
serena-ramley <sramley@gmail.com>
2020-01-08 21:50:51 -0800

    Sourced photos locally

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = ( {food, id, name, city, days_valid}) => {
     return (
         // Use tachyons to create green background and grow upon hover
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img className='br-pill' alt={food} src={`http://lorempixel.com/200/200/food/${id}`} width='200' height='200' />
+            <img className='br-pill' alt={food} src={require(`../photos/${id}.jpg`)} width='200' height='200' />
             <div>
                 <h2>{name}</h2>
                 <p>{city}</p>
