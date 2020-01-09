589873e94e9510906d1596feb4f8d74ebb224bce
serena-ramley <sramley@gmail.com>
2020-01-08 21:32:01 -0800

    Updates

---
+++ precache-manifest.a3ed39c1e9252252db28eb3201221bea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99c26ab2a8e5d1e6c242d8b991c3650",
+    "url": "/ReactGallery/index.html"
+  },
+  {
+    "revision": "4ee842c627cb88f6f88b",
+    "url": "/ReactGallery/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "42b4eb19aa40be6a9c7f",
+    "url": "/ReactGallery/static/css/main.fd301234.chunk.css"
+  },
+  {
+    "revision": "4ee842c627cb88f6f88b",
+    "url": "/ReactGallery/static/js/2.c71dd014.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ReactGallery/static/js/2.c71dd014.chunk.js.LICENSE"
+  },
+  {
+    "revision": "42b4eb19aa40be6a9c7f",
+    "url": "/ReactGallery/static/js/main.fc3e3064.chunk.js"
+  },
+  {
+    "revision": "1978a860ca40335d223c",
+    "url": "/ReactGallery/static/js/runtime-main.60f864a8.js"
+  },
+  {
+    "revision": "35418d8f6d04761a1df8a9e44e516bc6",
+    "url": "/ReactGallery/static/media/NexaBold.35418d8f.otf"
+  }
+]);
